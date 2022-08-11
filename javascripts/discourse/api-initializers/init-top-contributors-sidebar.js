cae87342bb3eae59a10382abc2dd01c2834b489b
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-11 13:53:45 -0700

    add aditional close btn

--- javascripts/discourse/api-initializers/init-top-contributors-sidebar.js
+++ javascripts/discourse/api-initializers/init-top-contributors-sidebar.js
@@ -4,9 +4,9 @@ import {
 
 export default apiInitializer("0.8", (api) => {
   api.onPageChange(() => {
-    console.log("api initalizer loaded");
     const additionalCloseBtn = document.querySelector(".close-composer-btn");
     const closeBtn = document.querySelector("#reply-control").querySelector(".close-composer-btn");
+    console.log(additionalCloseBtn, closeBtn);
     if (closeBtn && additionalCloseBtn) {
       console.log("checking if the event listenr is mounted");
       additionalCloseBtn.addEventListener('click', () => {
